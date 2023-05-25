522b02ee9784d336572c7b6ab85e6208ece22e43
Kobi Gurkan <kobigurk@gmail.com>
2023-05-25 14:26:08 +0300

    adds first commit

---
+++ run_bark.py
@@ -0,0 +1,24 @@
+from bark import SAMPLE_RATE, generate_audio, preload_models
+from scipy.io.wavfile import write as write_wav
+
+import modal
+
+bark_image = modal.Image \
+                    .debian_slim() \
+                    .apt_install("git") \
+                    .pip_install("git+https://github.com/suno-ai/bark.git") \
+                    .run_function(preload_models)
+
+stub = modal.Stub("bark-runner", image = bark_image)
+
+@stub.function(gpu="a100-20g")
+def talk(text):
+    from bark import generate_audio
+    audio_array = generate_audio(text)
+    return audio_array
+
+@stub.local_entrypoint()
+def main():
+    result = talk.call("♪ hello, is this me you're looking for ♪")
+    write_wav("bark_generation.wav", SAMPLE_RATE, result)
+
